b80e1286854e6ac92f943f23c219829d651e1094
sjm <2674114136@qq.com>
2022-08-05 22:53:28 +0800

    一部分选择元素

--- src/Home.vue
+++ src/Home.vue
@@ -1,81 +1,99 @@
 <template>
-    <div class="container">
-        <!-- 头部部分 -->
-        <Header></Header>
-        <!-- 主体部分 -->
-        <div class="main">
-            <!-- 左侧组件部分 -->
-            <div class="left">
-                <component-list></component-list>
-            </div>
-            <!-- 中间画布部分 -->
-            <div class="middle">
-                <operation></operation>
-            </div>
-            <!-- 右边组件部分 -->
-            <div class="right">
-                <setting></setting>
-            </div>
-        </div>
+  <div class="container">
+    <!-- 头部部分 -->
+    <Header></Header>
+    <!-- 主体部分 -->
+    <div class="main">
+      <!-- 左侧组件部分 -->
+      <div class="left">
+        <component-list></component-list>
+      </div>
+      <!-- 中间画布部分 -->
+      <div class="middle">
+        <operation></operation>
+        <div ref="write" class="write"></div>
+      </div>
+      <!-- 右边组件部分 -->
+      <div class="right">
+        <setting></setting>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Header from './components/Header.vue'
-    import ComponentList from './components/ComponentList.vue'
-    import Operation from './components/Operation.vue'
-    import Setting from './components/Setting.vue'
-    export default {
-    name: "Home",
-    components: { Header , ComponentList, Operation, Setting}
-}
+import Header from "./components/Header.vue";
+import ComponentList from "./components/ComponentList.vue";
+import Operation from "./components/Operation.vue";
+import Setting from "./components/Setting.vue";
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: "Home",
+  components: { Header, ComponentList, Operation, Setting },
+  setup() {
+    const write = ref(null);
+    onMounted(() => {
+    });
+    return {
+      write,
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-    * {
-        margin: 0;
-        padding: 0;
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  height: 753px;
+  background-color: #eeecf0;
+  background-size: cover;
+}
+.main {
+  display: flex;
+  justify-content: center;
+  padding: 0 15px;
+  overflow: hidden;
+  .left {
+    position: fixed;
+    top: 0px;
+    left: 15px;
+    margin-top: 65px;
+    width: 290px;
+    height: 673px;
+    background-color: #fff;
+    border-radius: 6px;
+    overflow: hidden;
+  }
+  .middle {
+    margin-top: 65px;
+    width: 876px;
+    height: 673px;
+    border-radius: 6px;
+    background-color: #fff;
+    .write {
+      width: 876px;
+      height: 623px;
+      position: absolute;
+      background-color: pink;
+      overflow: scroll;
     }
-    .container {
-        height: 753px;
-        background-color: #eeecf0;
-        background-size: cover;
-    }
-    .main {
-        display: flex;
-        justify-content: center;
-        padding: 0 15px;
-        overflow: hidden;
-        .left {
-            position: fixed;
-            top: 0px;
-            left: 15px;
-            margin-top: 65px;
-            width: 290px;
-            height: 673px;
-            background-color: #fff;
-            border-radius: 6px;
-            overflow: hidden;
-        }
-        .middle {
-            margin-top: 65px;
-            width: 876px;
-            height: 673px;
-            border-radius: 6px;
-            background-color: #fff;
-        }
-        .right {
-            position: fixed;
-            top: 0px;
-            right: 15px;
-            margin-top: 65px;
-            padding-bottom: 30px;
-            width: 290px;
-            height: 673px;
-            background-color: #fff;
-            border-radius: 6px;
-            overflow: hidden;
-            overflow-y:auto;
-        }
-    }
+  }
+  .right {
+    position: fixed;
+    top: 0px;
+    right: 15px;
+    margin-top: 65px;
+    padding-bottom: 30px;
+    width: 290px;
+    height: 673px;
+    background-color: #fff;
+    border-radius: 6px;
+    overflow: hidden;
+    overflow-y: auto;
+  }
+}
 </style>
