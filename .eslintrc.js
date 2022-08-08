27b907824fddbd9fed2ba7a30761bf539155cc59
Your Name <email@czy.com>
2022-08-08 21:07:11 +0800

    登录注册用户界面

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,31 @@
 module.exports = {
     root: true,
     env: {
-      node: true
+        node: true
     },
     'extends': [
-      'plugin:vue/essential',
-      'eslint:recommended'
+        'plugin:vue/essential',
+        'eslint:recommended'
     ],
     parserOptions: {
-      parser: '@babel/eslint-parser'
+        parser: '@babel/eslint-parser'
     },
     rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-       //在rules中添加自定义规则
-       //关闭组件命名规则
-      "vue/multi-word-component-names": "off",
-      "vue/no-multiple-template-root":'off' 
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        //在rules中添加自定义规则
+        //关闭组件命名规则
+        "vue/multi-word-component-names": "off",
+        "vue/no-multiple-template-root": 'off',
+        "vue/no-v-model-argument": "off"
     },
-    overrides: [
-      {
+    overrides: [{
         files: [
-          '**/__tests__/*.{j,t}s?(x)',
-          '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            '**/__tests__/*.{j,t}s?(x)',
+            '**/tests/unit/**/*.spec.{j,t}s?(x)'
         ],
         env: {
-          jest: true
+            jest: true
         }
-      }
-    ]
-  }
-  
-  
+    }]
+}
